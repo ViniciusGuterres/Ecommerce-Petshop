02738f49b70d63ea84f89f19c758a58cec38bff2
vinicius guterres <vinicius@spacecom.com.br>
2023-06-13 01:08:51 -0300

    Adding dataKey props and adding component documentation

--- web/src/components/Input.jsx
+++ web/src/components/Input.jsx
@@ -1,18 +1,23 @@
 /**
- * @function Input - 
- * @param {string|number} value -
- * @param {function=} onChange -
- * @param {string=} placeholder -
- * @param {string} class - 
+ * @function Input - Input component
+ * @param {string|number} value - The input value
+ * @param {function=} onChange - Function to be called on the input change
+ * @param {string=} placeholder - The input placeholder string
+ * @param {string=} cssClass - The tailwind class
+ * @param {string=} dataKey - The input key name
  */
-function Input({ value, onChange, placeholder, isReadOnly, cssClass, type }) {
+function Input({ value, onChange, placeholder, cssClass, type, dataKey }) {
 
     // Functions
+    /**
+     * @function handleOnChangeValue - Will get the on change value and passed to the onChange props functions if has it
+     * @param {object} evt - The input change object 
+     */
     const handleOnChangeValue = (evt) => {
         const eventValue = evt.target.value;
 
         if (typeof onChange == 'function') {
-            onChange(eventValue);
+            onChange(eventValue, dataKey);
         }
     }
 
