d4f6503e282760e86d52335432c8718554e494fb
vinicius guterres <vinicius@spacecom.com.br>
2023-06-16 12:31:04 -0300

    Initing Product and HomeProduct components

---
+++ web/src/components/Product.jsx
@@ -0,0 +1,8 @@
+function Product({ name, description, image, price, rating }) {
+
+    return (
+        <h1> Produto </h1>
+    );
+}
+
+export default Product;
---
+++ web/src/views/HomeProducts.js
@@ -0,0 +1,13 @@
+// Components
+import Product from "../components/Product";
+
+function HomeProducts() {
+    return (
+        <div>
+            <h1>Home de produtos</h1>
+            <Product />
+        </div>
+    );
+}
+
+export default HomeProducts;
