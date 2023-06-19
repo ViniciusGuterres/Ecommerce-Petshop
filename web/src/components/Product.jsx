d393cdc1f0b22f0ead5b83438397828dd3d2789d
vinicius guterres <vinicius@spacecom.com.br>
2023-06-19 00:31:38 -0300

    Adding Product component

--- web/src/components/Product.jsx
+++ web/src/components/Product.jsx
@@ -1,7 +1,85 @@
-function Product({ name, description, image, price, rating }) {
+/**
+ * @function Product - The product container with some information and "buy" button
+ * @param {string} name - The product name
+ * @param {string} image - The product Base64 image
+ * @param {number} price - The product price
+ * @param {number} rating - The product rating based on clients experiences
+ * @param {function} onClickToBuyProductFunction - The function to handle when click to buy button
+ */
+function Product({ name, image, price, rating, onClickToBuyProductFunction }) {
+    // Vars
+    const priceFormattedToDecimals = price.toFixed(2)?.toString() || '';
+    const priceFormatted = price ? `R$: ${priceFormattedToDecimals?.replace('.', ',')}` : '';
+
+    // Adding fake rating for while
+    rating = 3;
+
+    // Functions
+    /**
+     * @function Components/Product/buildRatingElement - Will build the rating start element
+     * @returns {Element} - The product's start rating elements
+     */
+    const buildRatingElement = () => {
+        return (
+            <div className="flex items-center mt-2.5 mb-5">
+                <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+
+                <span
+                    className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3"
+                >
+                    {rating}
+                </span>
+            </div>
+        );
+    };
 
     return (
-        <h1> Produto </h1>
+        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                {/* Product img */}
+                <img
+                    className="p-8 rounded-t-lg"
+                    src={image}
+                    alt='pet shop ecommerce product'
+                />
+            </a>
+
+            <div className="px-5 pb-5">
+                {/* Name */}
+                <a href="#">
+                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                        {name}
+                    </h5>
+                </a>
+
+                {/* Rating */}
+                {
+                    rating
+                        ?
+                        buildRatingElement()
+                        :
+                        null
+                }
+
+                <div className="flex items-center justify-between">
+                    {/* Price */}
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                        {priceFormatted}
+                    </span>
+
+                    {/* Add to cart button */}
+                    <button
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                        Comprar
+                    </button>
+                </div>
+            </div>
+        </div>
     );
 }
 
