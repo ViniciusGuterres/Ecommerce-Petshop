e3ae27fd4ebb683ff4ba78a400cf783c410e7240
vinicius guterres <vinicius@spacecom.com.br>
2023-06-20 22:32:22 -0300

    Update Header.jsx

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -53,7 +53,7 @@ function Header() {
                     <div className="flex items-center lg:order-2">
                         {/* Login button */}
                         <a
-                            href='#'
+                            href='/login'
                             className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
                         >
                             Logar
@@ -61,7 +61,7 @@ function Header() {
 
                         {/* Register */}
                         <a
-                            href="#"
+                            href="/customerCrud"
                             className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
                             Cadastre-se
                         </a>
