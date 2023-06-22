d1f5a5630827e397bdd087b26ad109f82609ca15
vinicius guterres <vinicius@spacecom.com.br>
2023-06-22 20:50:22 -0300

    Adjusting view css

--- web/src/views/HomeProducts.js
+++ web/src/views/HomeProducts.js
@@ -116,27 +116,6 @@ function HomeProducts() {
         return result;
     }
 
-    const buildSearchFilter = () => {
-        return (
-            <form className="flex items-center w-60">
-                <div className="relative w-full">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"></path></svg>
-                    </div>
-
-                    <input
-                        type="text"
-                        id="simple-search"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Procurar produto"
-                        value={inputFilterValue}
-                        onChange={evt => handleChangeInputFilter(evt)}
-                    />
-                </div>
-            </form>
-        );
-    }
-
     const buildModalFilter = () => {
         const sortOptionsDefaultClass = 'font-medium text-gray-900 block px-4 py-2 text-sm cursor-pointer';
         const sortOptionsSelectedClass = 'text-gray-900 block px-4 py-2 text-sm text-primary-700';
@@ -204,9 +183,23 @@ function HomeProducts() {
 
     const buildFilters = () => {
         return (
-            <div className="flex gap-2 w-screen items-center">
-                {buildSearchFilter()}
-                {buildModalFilter()}
+            <div className="bg-white shadow-sm sticky top-0">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-1 md:py-4">
+                    <div className="flex items-center justify-between md:justify-start">
+                        <div className="flex items-center space-x-4">
+                            {buildModalFilter()}
+
+                            <input
+                                type="text"
+                                id="simple-search"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Procurar produto"
+                                value={inputFilterValue}
+                                onChange={evt => handleChangeInputFilter(evt)}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
